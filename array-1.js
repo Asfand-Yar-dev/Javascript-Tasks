c097d44359a4c9b1db2e3ec46326cdb8ea7de01c
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-14 20:41:13 +0500

    add files

---
+++ array-1.js
@@ -0,0 +1,15 @@
+// let arr = [1,2,3,4,5,6,7]
+
+// // console.log("your length is:",arr.length);
+
+// // arr.push(9);
+
+
+// console.log("your array is:", arr[4]);
+
+let arr = [1,2,3,4,5,6,7];
+
+ // returns the index of the element 3 in the array
+arr.push(8) // adds 8 to the end of the array
+
+console.log(arr.indexOf(3))
--- arrowFunction.js
+++ arrowFunction.js
@@ -1,5 +1,7 @@
 // Arrow function to add two numbers
-const add = (a, b) => a + b;
+// const add = (a, b) => a + b;
+// console.log(add(3, 5)); // Output: 8
 
-// Using the function
-console.log(add(3, 5)); // Output: 8
+let mul = (a,b) => a*b;
+
+console.log(mul(2,3));
