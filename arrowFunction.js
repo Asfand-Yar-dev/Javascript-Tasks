1c5d6dc605da260091cb610dc418967e841f8769
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-15 00:00:15 +0500

    commit

--- arrowFunction.js
+++ arrowFunction.js
@@ -5,3 +5,9 @@
 let mul = (a,b) => a*b;
 
 console.log(mul(2,3));
+
+/*arrow funtion are just short syntax of returning arguments in function
+
+function add (a,b)
+    return a+b
+they are all same */
