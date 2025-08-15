f667a1f8d55f1ad3c0eb2ee7a5368640bff0748a
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-16 00:20:20 +0500

    prac-2

--- array-prac1.js
+++ array-prac1.js
@@ -1,14 +1,14 @@
-// const arr  = [1,2,3,5,6];
-// arr.splice(3,0,4)
-// console.log(arr);
+const arr  = [1,2,3,5,6];
+arr.splice(3,0,4)
+console.log(arr);
 
 
-// const N1 = [1,2,3,4,5];
-// const N2 = ['a','b','c','d','e']
+const N1 = [1,2,3,4,5];
+const N2 = ['a','b','c','d','e']
 
-// const check = N1.concat(6);
+const check = N1.concat(6);
 
-// console.log(check);
+console.log(check);
 
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
