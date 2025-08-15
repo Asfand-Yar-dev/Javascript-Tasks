2df1b9706f4eed52c947e379c8748d8050cc728b
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-16 00:13:46 +0500

    new file

---
+++ array-prac1.js
@@ -0,0 +1,11 @@
+// const arr  = [1,2,3,5,6];
+// arr.splice(3,0,4)
+// console.log(arr);
+
+
+const N1 = [1,2,3,4,5];
+const N2 = ['a','b','c','d','e']
+
+const check = N1.concat(6);
+
+console.log(check);
