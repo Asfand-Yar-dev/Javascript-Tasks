991c14491cf768f1265e42b848b0ef78c4226d1e
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-17 00:04:59 +0500

    update files

--- swapping.js
+++ swapping.js
@@ -3,9 +3,7 @@
 let a = prompt('Enter the first variable: ');
 let b = prompt('Enter the second variable: ');
 
-
 let temp;
-
 
 temp = a;
 a = b;
--- to-String.js
+++ to-String.js
@@ -1,6 +1,6 @@
-// let x = 12345
-// let text = x.toString()
-// console.log(text); //converts any number to string
+let a = 12345
+let msg = x.toString()
+console.log(msg); //converts any number to string
 
 //showing number in decimal (2) and hexadecimal (16)
 let x = 12345
