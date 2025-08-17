d9112e82fa99aa178cbedf01e03a7e669ba089ee
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-18 01:03:49 +0500

    add files

--- functions.js
+++ functions.js
@@ -1,40 +1,29 @@
-// function sum(a,b){
-//     return a+b;
-// }
-// let a =10;
-// let b =20;
-// console.log("Your sum is",sum(a,b));
+//function prac-1
+function sum(a,b){
+     return a+b;
+ }
+ let a =10;
+ let b =20;
+ console.log("Your sum is",sum(a,b));
+
+//function prac-2
+
+ let x = myfunction(10,2)
+
+ function myfunction(a,b){
+     return a+b;
+ }
+ console.log('Your number is',x);
+
+//function prac-3
+ let name="Asfand";
+ function showmessage(){
+     console.log('Hello', name);
+ }
+ showmessage();
 
 
-
-
-
-
-
-// let x = myfunction(10,2)
-
-// function myfunction(a,b){
-//     return a+b;
-// }
-// console.log('Your number is',x);
-
-
-
-
-
-
-
-// let name="Asfand";
-// function showmessage(){
-//     console.log('Hello', name);
-// }
-// showmessage();
-
-
-
-
-
-
+//function prac-4
 let cname ="Asfand Yar"
 
 function noshow(){
@@ -43,7 +32,7 @@ function noshow(){
 }
 noshow();
 
-
+//function prac-5
 function loginUserMessage(username = "sam"){
     if(!username){
         console.log("PLease enter a username");
