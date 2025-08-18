4a91dbeafa1e1e7fc50ae41967b873cab30258e8
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-19 00:14:28 +0500

    add file

---
+++ try-catch-error.js
@@ -0,0 +1,22 @@
+setTimeout(()=>{
+    console.log("Hello World")
+},1000)
+
+try{
+    console.log(Asfand)
+}
+catch(error){
+    console.log(error.message)
+}
+
+setTimeout(()=>{
+    console.log("Hello World...1")
+},2000)
+
+setTimeout(()=>{
+    console.log("Hello World...2")
+},3000)
+
+setTimeout(()=>{
+    console.log("Hello World...3")
+},4000)
