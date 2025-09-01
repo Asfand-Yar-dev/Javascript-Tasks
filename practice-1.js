111bc8bc33e045d9b699310f12788611ff19c33f
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-09-02 00:26:22 +0500

    Added file

---
+++ practice-1.js
@@ -0,0 +1,130 @@
+// let name = 'Asfand Yar'
+// let age = 22
+// let isStudent = true
+// console.log(name,age,isStudent);
+
+// let a = 10;
+// let b = 20;
+// [a,b] = [b,a];
+// console.log(`a: ${a}, b: ${b}`);
+
+// let str = "123"
+// let num = Number(str)
+// let result = num + 10
+// console.log(result);
+
+// let alp = false;           // boolean false
+// let num = Number(alp);     // 0
+// let str = String(num);     // "0"
+// console.log(str);          // "0"
+
+// let triangle = prompt("Enter your Triangle")
+// let rectangle = prompt("Enter your Rectangle")
+// let result = triangle * rectangle
+// alert(result);
+
+// let a = 10
+// a++
+// let result = a * 5
+// console.log(result);
+
+// let age = prompt("Enter your age");
+// if(age<18){
+//     console.log("Child")
+// }
+// else if(age==18){
+//     console.log('Minor');
+// }
+// else {
+//     console.log('Adult');
+// }
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let number = Math.max(a,b,c)
+// console.log('Maximum is:',number);
+
+// let num1 = prompt("Enter your number")
+// if(num1%3==0){
+//     console.log('number is 3');
+// }
+// else{
+//     console.log('number is 5');
+// }
+
+// let user = "user"
+// let pwd = 1234
+
+// let check = prompt("Enter your username")
+// let check2 = prompt("Enter your password")
+
+// if(check==user && check2==pwd){
+//     alert("Welcome User!");
+// }
+// else{
+//     alert('Login Failed');
+// }
+
+// let nickname = prompt("Enter your name")
+// nickname = nickname ?? "Guest"
+// console.log('Hello', nickname);
+
+// for (let i = 0;i<=20;i++){
+//     console.log(i);
+// }
+
+// let num = prompt("Enter your number")
+// for (let i=1;i<=10;i++){
+//     console.log(num,'*',i,'=',num*i )
+// }
+
+// let sum = 0
+// for (let i = 0;i<=100;i++){
+//     sum +=i
+// }
+// console.log('Sum is:',sum);
+
+// let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// switch (new Date().getDay()) {
+//     case 0:
+//         console.log(weekDays[0]);
+//         break;
+//     case 1:
+//         console.log(weekDays[1]);
+//         break;
+//     case 2:
+//         console.log(weekDays[2]);
+//         break;
+//     case 3:
+//         console.log(weekDays[3]);
+//         break;
+//     case 4:
+//         console.log(weekDays[4]);
+//         break;
+//     case 5:
+//         console.log(weekDays[5]);
+//         break;
+//     case 6:
+//         console.log(weekDays[6]);
+//         break;
+// }
+
+// function check(num){
+//     if(num%2==0){
+//         console.log('Even');
+//     }
+//     else{
+//         console.log('Odd');
+//     }
+// }
+// check(4);
+
+// function radius(radius){
+//     Area = Math.PI * radius * radius;
+//     return Area
+// }
+// console.log('Area is:',radius(5));
+
+// const radius1 = (radius) => (Math.PI * radius * radius);
+// console.log('Area is:',radius1(10));
