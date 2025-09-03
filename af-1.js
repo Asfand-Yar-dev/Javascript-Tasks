3ce9fc5a476417511a556a9f2b9ea3bf27afda9c
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-09-03 20:24:35 +0500

    Practice Questions

--- af-1.js
+++ af-1.js
@@ -1,63 +1,63 @@
-// // // // // // // // // // Task-1 => convert into arrow functions
-// // // // // // // // // // function square(n) {
-// // // // // // // // // //   return n * n;
-// // // // // // // // // // }
+        //   Task-1 => convert into arrow functions
+        //   function square(n) {
+        //     return n * n;
+        //   }
 
-// // // // // // // // // let square = (n) =>  (n*n)
+        //  let square = (n) =>  (n*n)
 
-// // // // // // // // // console.log(square(2));
-
-
-// // // // // // // // // let number  = prompt("Enter your number")
-
-// // // // // // // // const check = (number) => (
-// // // // // // // //     (number%2==0)? 'Even':
-// // // // // // // // 'Odd'
-// // // // // // // // )
-
-// // // // // // // // console.log(`Your number is:`+check(10));
-
-// // // // // // // let n = [1,2,3,4,5]
-
-// // // // // // // const check = () => (n.map(n => n + n))
-
-// // // // // // // console.log(check());
-
-// // // // // // let ages = [12, 19, 22, 15, 30];
-
-// // // // // // const crude = () => (ages.filter(ages=> ages>18 ))
-
-// // // // // // console.log(crude());
+        //  console.log(square(2));
 
 
+        //  let number  = prompt("Enter your number")
 
-// // // // // const check = (n) => (n*n)
+        // const check = (number) => (
+        //     (number%2==0)? 'Even':
+        // 'Odd'
+        // )
 
-// // // // // console.log(check(8))
+        // console.log(`Your number is:`+check(10));
 
+    //    let n = [1,2,3,4,5]
 
-// // // // const checknum = (a,b) => (
-// // // //     (a>b) ? a :
-// // // //     b
-// // // // )
+    //    const check = () => (n.map(n => n + n))
 
-// // // // console.log(checknum(10,20));
+    //    console.log(check());
 
-// // // const name = (name) => (name.length)
+    //   let ages = [12, 19, 22, 15, 30];
 
-// // // console.log(name('abcdefghijklmnopqrstuvwxyz'));
+    //   const crude = () => (ages.filter(ages=> ages>18 ))
 
-// // let name =prompt("Enter your name")
-// // const name1 = () => ("Hello" +" "+  name)
-
-// // console.log(name1());
+    //   console.log(crude());
 
 
-// const name1 = (name) => (name.split("").reverse().join(""))
-// console.log(name1('Hello'));
 
-const check = (age) => (
-    (age>18) ? 'Adult':
-    'Minor'
-)
-console.log(check(17));
+    //  const check = (n) => (n*n)
+
+    //  console.log(check(8))
+
+
+    // const checknum = (a,b) => (
+//     (a>b) ? a :
+//     b
+// )
+
+// console.log(checknum(10,20));
+
+//    const name = (name) => (name.length)
+
+//    console.log(name('abcdefghijklmnopqrstuvwxyz'));
+
+//   let name =prompt("Enter your name")
+//   const name1 = () => ("Hello" +" "+  name)
+
+//   console.log(name1());
+
+
+//  const name1 = (name) => (name.split("").reverse().join(""))
+//  console.log(name1('Hello'));
+
+// const check = (age) => (
+//     (age>18) ? 'Adult':
+//     'Minor'
+// )
+// console.log(check(17));
