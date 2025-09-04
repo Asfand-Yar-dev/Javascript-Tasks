3824b6bc719e879cacaab9d6c265080d80ae56fb
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-09-04 21:38:53 +0500

    Update

--- Task.js
+++ Task.js
@@ -1,5 +1,5 @@
- var a= 20;
-const b=30;
+ var b= 20;
+const c=30;
  console.log("20"+"89");
 
 //type of
@@ -8,10 +8,7 @@ console.log(typeof a);
 
 
 
-let name = "Asfand          ";
-
-console.log( `hello ${1}` ); // ?
-
-console.log( `hello ${"name"}` ); // ?
-
-console.log( `hello ${name}` ); // ?
+let name = "Asfand";
+console.log( `hello ${1}` ); // hello 1
+console.log( `hello ${"name"}` ); // hello name
+console.log( `hello ${name}` ); // hello Asfand
